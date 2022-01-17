0293d7dd9107652d4929c6a78393dfca50deddcf
S2Onion <ksskss665@naver.com>
2022-01-17 20:01:52 +0900

    Joi 설치 및 환경변수 Validation 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import * as Joi from 'joi'; // 자바 스크립트로 작성되어 있음
 import { RestaurantsModule } from './restaurants/restaurants.module';
+
+console.log(Joi);
 
 @Module({
   imports: [
@@ -10,19 +13,28 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
       isGlobal: true,
       envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
       ignoreEnvFile: process.env.NODE_ENV === 'prod', // 프로젝트 배포시엔 환경파일 .env 을 넘기지 않도록 적용
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev', 'prod', 'test').required(),
+        PORT: Joi.number().default(3000),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+      }),
     }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
     TypeOrmModule.forRoot({
-      "type": "postgres",
-      "host": process.env.DB_HOST,
-      "port": +process.env.DB_PORT,
-      "username": process.env.DB_USERNAME,
-      "password": process.env.DB_PASSWORD,
-      "database": process.env.DB_NAME,
-      "synchronize": true,
-      "logging": true
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true,
+      logging: true,
     }),
     RestaurantsModule,
   ],
