2a5d9d1ac31b93f1f35ce651ba57c9753916aa69
S2Onion <ksskss665@naver.com>
2022-01-20 20:59:43 +0900

    계정 생성 로직 수정

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,4 +1,4 @@
-import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { CreateAccountInput, CreateAccountOutput } from './dtos/create-account.dto';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
@@ -7,24 +7,13 @@ import { UsersService } from './users.service';
 export class UsersResolver {
     constructor(private readonly usersService: UsersService) { }
 
-    @Query(() => Boolean)
-    hi() {
-        return true;
-    }
-
     @Mutation(() => CreateAccountOutput)
     async createAccount(@Args('input') createAccountInput: CreateAccountInput): Promise<CreateAccountOutput> {
         try {
-            const error = await this.usersService.createAccount(createAccountInput);
-            if (error) {
-                return {
-                    ok: false,
-                    error,
-                }
-            }
-
+            const {ok, error} = await this.usersService.createAccount(createAccountInput);
             return {
-                ok: true
+                ok,
+                error
             }
         } catch (e) {
             return {
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -8,18 +8,19 @@ import { User } from './entities/user.entity';
 export class UsersService {
     constructor(@InjectRepository(User) private readonly usersRepository: Repository<User>) { }
 
-    async createAccount({ email, password, role }: CreateAccountInput): Promise<string | undefined> {
+    async createAccount({ email, password, role }: CreateAccountInput): Promise<{ ok: boolean, error?: string }> {
         try {
             // Email 중복 확인
             const exists = await this.usersRepository.findOne({ email });
             if (exists) { // Email 정보가 DB에 있을 경우 에러 처리
-                return 'There is a user with that email already';
+                return { ok: false, error: 'There is a user with that email already' };
             }
 
             // 계정 생성 및 비밀번호 암호화
             await this.usersRepository.save(this.usersRepository.create({ email, password, role }))
+            return { ok: true };
         } catch (e) {
-            return 'Couldn\'t create account';
+            return { ok: false, error: 'Couldn\'t create account' };
         }
     }
 }
