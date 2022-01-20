48ada9165ac7c6e76ebae6cefefefe3b6ce81f62
S2Onion <ksskss665@naver.com>
2022-01-20 21:10:06 +0900

    패스워드 Hash 처리

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,6 +1,8 @@
+import { InternalServerErrorException } from "@nestjs/common";
 import { Field, InputType, ObjectType, registerEnumType } from "@nestjs/graphql";
+import * as bcrypt from 'bcrypt';
 import { CoreEntity } from "src/common/entities/core.entity";
-import { Column, Entity } from "typeorm";
+import { BeforeInsert, Column, Entity } from "typeorm";
 
 enum UserRole {
     Owner,
@@ -26,4 +28,14 @@ export class User extends CoreEntity {
     @Field(type => UserRole)
     @Column({ type: 'enum', enum: UserRole })
     role: UserRole;
+
+    @BeforeInsert()
+    async hashPassword(): Promise<void> {
+        try {
+            this.password = await bcrypt.hash(this.password, 10);
+        } catch (e) {
+            console.error(e);
+            throw new InternalServerErrorException();
+        }
+    }
 }
